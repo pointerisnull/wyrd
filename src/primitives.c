ed1dd4ea3af82bd0867db0ed5a2c36f7e6847e65
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-23 11:23:47 -0500

    bug fix

--- src/primitives.c
+++ src/primitives.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 
 Vertex *create_verticies(int count) {
-    Vertex verticies = malloc(sizeof(Vertex)*count)
+    Vertex *verticies = malloc(sizeof(Vertex)*count);
     return verticies;
 }
 
 void populate_verticies(Vertex *verts, int count, int *positions) {
     int offset = 0;
     for(int i = 0; i < count; i++) {
-        verts[i]->x = positions[i+offset];
-        verts[i]->y = positions[i+offset+1];
-        verts[i]->z = positions[i+offset+2];
-        offset =+ 3;
+        verts[i].x = positions[offset];
+        verts[i].y = positions[offset+1];
+        verts[i].z = positions[offset+2];
+        offset += 3;
     }
 }
