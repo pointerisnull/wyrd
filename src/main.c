d5918e97530101c94760354d82c4ab1e2ec4a8c8
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-02-10 22:17:10 -0500

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -7,14 +7,7 @@
 #endif
 
 int main(int argc, char **argv) {
-  /*
-  int count = 4;
-  Vertex *verts = create_verticies(count);
-  int positions[] = {3,4,5,1,2,3,9,8,7,11};
-  populate_verticies(verts, count, positions);
-  printf("Vertex Count: %d\n", count);
-  printf("Vertex 3 position - x: %d y: %d \n", verts[2].x, verts[2].y);
-  */
+
   //Start up SDL and create window
   Window *win = malloc(sizeof(Window));
 
